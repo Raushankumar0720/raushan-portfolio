a3df4f4fa59370ee3a02baf70d59ac367e2587bd
Raushankumar0720 <raushan.singh.cg@gmail.com>
2026-04-18 00:05:05 +0530

    feat: integrate socialLinks dataset

---
+++ src/data/socialLinks.js
@@ -0,0 +1,48 @@
+import { FaGithub, FaLinkedinIn, FaXTwitter, FaYoutube } from 'react-icons/fa6';
+import { SiLeetcode, SiSololearn } from 'react-icons/si';
+
+export const socialLinks = [
+  {
+    name: 'GitHub',
+    url: 'https://github.com/Raushankumar0720',
+    icon: FaGithub,
+    label: 'GH',
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/raushan150720/',
+    icon: FaLinkedinIn,
+    label: 'LI',
+  },
+  {
+    name: 'Twitter / X',
+    url: 'https://x.com/RaushanKum68222',
+    icon: FaXTwitter,
+    label: 'X',
+  },
+  {
+    name: 'YouTube',
+    url: 'https://www.youtube.com/@codePressure',
+    icon: FaYoutube,
+    label: 'YT',
+  },
+  {
+    name: 'LeetCode',
+    url: 'https://leetcode.com/u/raushankumar150720/',
+    icon: SiLeetcode,
+    label: 'LC',
+  },
+  {
+    name: 'Sololearn',
+    url: 'https://www.sololearn.com/en/profile/34907165',
+    icon: SiSololearn,
+    label: 'SL',
+  },
+];
+
+export const contactInfo = {
+  email: 'raushan.singh.cg@gmail.com',
+  location: 'Ahmedabad, India',
+  availability: 'Open to Work',
+  roles: ['Full-Time', 'Internship', 'Freelance', 'Remote'],
+};
